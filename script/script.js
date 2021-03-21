b3f80dbddb8559d5bec2ce7c32728d7d71fee0ad
20170375 <62216628+20170375@users.noreply.github.com>
2021-03-21 23:02:36 +0900

    commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
+function go_home(){
+    document.location.href="../html/home.html";
+}
 
+function go_top(){
+    document.location="#headline";
+}
+
+function search(){
+    url="https://www.google.com/search?q=";
+    url += document.getElementById("search_bar").value;
+    window.open(url);
+}
