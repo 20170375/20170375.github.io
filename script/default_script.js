eea3c395ba519f21d87ca7ecc13f4acfe73eccda
20170375 <62216628+20170375@users.noreply.github.com>
2021-03-26 03:57:58 +0900

    bug fix

--- script/default_script.js
+++ script/default_script.js
@@ -1,10 +1,13 @@
-function go_top(){
-    document.location="#headline";
+function menu_focus(menu){
+    menu.classList.toggle("menu_focus");
 }
 function search(){
     url="https://www.google.com/search?q=";
     url += document.getElementById("search_bar").value;
     window.open(url);
+}
+function go_top(){
+    document.location="#headline";
 }
 
 var a_list = document.getElementsByTagName("a");
@@ -13,3 +16,11 @@ for(var i=0; i<a_list.length; i++){
         a_list[i].setAttribute("target", "_blank");
     }
 }
+
+var menu_list = document.getElementsByClassName("menu_element");
+for(var i=0; i<menu_list.length; i++){
+  menu_list[i].setAttribute("onmouseover", "menu_focus(this)");
+  menu_list[i].setAttribute("onmouseout", "menu_focus(this)");
+}
+
+document.getElementById("go_top").setAttribute("onclick", "go_top()");
--- script/index_script.js
+++ script/index_script.js
@@ -1,14 +1,5 @@
-function menu_focus(menu){
-    menu.classList.toggle("menu_focus");
-}
 function block_focus(block){
     block.classList.toggle("block_focus");
-}
-
-var menu_list = document.getElementsByClassName("menu_element");
-for(var i=0; i<menu_list.length; i++){
-  menu_list[i].setAttribute("onmouseover", "menu_focus(this)");
-  menu_list[i].setAttribute("onmouseout", "menu_focus(this)");
 }
     
 var blocks = document.getElementsByClassName("blocks");
