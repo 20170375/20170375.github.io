fef2fbba2bfbd13c6310a3893eba3d5b42b2bc7d
20170375 <62216628+20170375@users.noreply.github.com>
2021-07-19 23:29:30 +0900

    md viewer update

--- js/loadmd.js
+++ js/loadmd.js
@@ -1,25 +1,30 @@
+// load marked.js
+var script = document.createElement('script');
+script.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
+document.head.append(script);
+
 // view md file from json
 var md_list = document.querySelector(".md_list");
 function load_md(json) {
     jQuery.getJSON(json, function (data) {
         for ( var i=0; i<data.length; ++i ) {
-            var md_file = document.createElement('div');
-            md_file.style.textAlign = "center";
-            var h2 = document.createElement('h2');
-            h2.innerText = data[i].name;
-            h2.style.alignSelf = "left";
-            md_list.append(h2);
-            var iframe = document.createElement('iframe');
-            iframe.style.width = "80%";
-            iframe.style.height = "700px";
-            iframe.style.border = "none";
-            iframe.style.backgroundColor = "#60646c";
-            iframe.src = data[i].href;
-            md_file.append(iframe);
-            if ( i != data.length-1 ) {
-                md_file.innerHTML += "<hr color=\"#30343c\" background-color=\"#30343c\">";
-            }
-            md_list.append(md_file);
+            jQuery.get(data[i].href, function (data) {
+                var div = document.createElement('div')
+                md_list.append(div);
+                div.style.margin = "20px";
+                div.style.padding = "10px";
+                div.style.backgroundColor = "#282d34";
+                div.innerHTML = marked(data);
+                var br = document.createElement('br');
+                md_list.append(br);
+                var hr = md_list.querySelectorAll("div > hr");
+                for ( var i=0; i<hr.length; ++i ) {
+                    hr[i].style.backgroundColor = "#646464";
+                    hr[i].style.margin = "0";
+                    hr[i].style.height = "2.5px";
+                    hr[i].style.borderWidth = "0px";
+                }
+            });
         }
     });
 };
